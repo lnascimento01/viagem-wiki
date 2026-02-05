e0996e5d1e2fdb05295ac7d9026915ec6d5963f3
Leandro Nascimento <leandro.nascimento@conectaonline.net.br>
2026-02-05 16:43:18 -0300

    Config: baseUrl GitHub Pages (lnascimento01/viagem-wiki)

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "lnascimento01.github.io/viagem-wiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
